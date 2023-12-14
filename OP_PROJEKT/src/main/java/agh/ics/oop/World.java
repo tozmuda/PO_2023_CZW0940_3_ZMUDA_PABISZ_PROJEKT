4eae37f7ae6ff5db08991f920669c87ebfdaaa38
Magdamini <mpabisz@student.agh.edu.pl>
2023-12-14 10:50:35 +0100

    Test 2

--- OP_PROJEKT/src/main/java/agh/ics/oop/World.java
+++ OP_PROJEKT/src/main/java/agh/ics/oop/World.java
@@ -3,5 +3,7 @@ package agh.ics.oop;
 public class World {
     public static void main(String[] args) {
         System.out.println("Helllo world");
+
+        System.out.println("hello 2");
     }
 }
